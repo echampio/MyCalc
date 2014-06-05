55a10f62bdc0fd6e5bcfa606adfef67d99156d25
Chris Champion <cchampio@gmail.com>
2014-06-04 21:35:14 -0500

    added class calc.cs

---
+++ MyCalc/Calc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCalc
+{
+    public class Calc
+    {
+        public double add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
