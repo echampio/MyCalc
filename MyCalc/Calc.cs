01f87e6c9a692b6dbe0e1f8f75ded4afa6c8e589
Ella Champion <arachampion@gmail.com>
2014-06-07 15:17:13 -0500

    Implemented Calc.sub method.
    Implemented Calc.sub method with unit tests.

--- MyCalc/Calc.cs
+++ MyCalc/Calc.cs
@@ -6,5 +6,10 @@
         {
             return x + y;
         }
+
+        public double sub(double x, double y)
+        {
+            return x-y;
+        }
     }
 }
---
+++ MyCalcTests/CalcTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalc;
+
+namespace MyCalcTests
+{
+    ///This is a test class for CalcTest and is intended
+    ///to contain all CalcTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class CalcTest
+    {
+        /// <summary>
+        ///A test for Calc Constructor
+        ///</summary>
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Calc c = new Calc();
+            Assert.IsTrue(true);
+        }
+
+        /// <summary>
+        ///A test for the add method.
+        ///</summary>
+        [TestMethod()]
+        public void addTest()
+        {
+            Calc c = new Calc();
+            Assert.AreEqual<double>(6.0D, c.add(1, 5));
+        }
+
+        /// <summary>
+        /// A test for the sub method.
+        /// </summary>
+        [TestMethod()]
+        public void subTest()
+        {
+            Calc c = new Calc();
+            Assert.AreEqual<double>(4.0D, c.sub(5, 1));
+        }
+    }
+}
